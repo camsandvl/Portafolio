048db834a752c95933b221db935a2f1cbcbe5bf0
camsandvl <san24358@uvg.edu.gt>
2026-07-03 18:41:31 -0600

    config fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portafolio-Camila-Sandoval/',
+  base: '/Portafolio/',
   build: { outDir: 'docs' },
 })
